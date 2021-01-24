90aed2b4fa9245d841866ee05c22e55192687275
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-23 23:37:51 -0500

    added functionality to home, product component, and minor styling to center products.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import Product from '../components/Product';
 
 export default function Home() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const response = await fetch('http://localhost:1337/products');
+            const data = await response.json();
+            setProducts(data)
+        }
+
+        getProducts()
+    }, []);
+
+
+
+
     return (
-        <div className = 'home'>
-            <h1> home, test</h1>
+        <div className = 'product-list'>
+            {products.map(product => (
+                
+                    <Product
+                        title={product.title}
+                        description={product.description}
+                        url = {product.image && product.image.url}
+                    />
+                
+            ))}
 
         </div>
     );
