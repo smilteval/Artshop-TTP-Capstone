2ad3ec52fb2dd8e78769f802c3af995e34db663c
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-25 13:06:11 -0500

    created a single product component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/NavigationBar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import SingleProduct from './pages/SingleProduct';
 // import Reports from './pages/Reports';
 // import Products from './pages/Products';
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path ="/products/:product_id" component={SingleProduct}/>
           {/* <Route path='/reports' component={Reports} /> */}
           {/* <Route path='/products' component={Products} /> */}
         </Switch>
---
+++ src/pages/SingleProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SingleProduct() {
+    return (
+        <div>
+            <h1>Product</h1>
+        </div>
+    )
+}
