5b8371338edb3a98ff744db33a34058dde53aa1e
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-25 18:01:21 -0500

    got product's info and displayed it on the screen

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from "react-router-dom"
 
 export default function SingleProduct() {
-    return (
-        <div>
-            <h1>Product</h1>
-        </div>
-    )
+    const productId = useParams().id;
+
+  const [product, setProduct] = useState([]);
+
+  //fetching one product's data from Strapi
+  useEffect(() => {
+    const getProduct = async () => {
+      const response = await fetch(
+        `https://ttp-art-store.herokuapp.com/products/${productId}`
+      );
+      const data = await response.json();
+      setProduct(data);
+    };
+    getProduct();
+  }, []);
+
+  console.log(product);
+
+  return (
+    <div className="single-product-page">
+      <div className="product-image">
+        <img src={product.image && product.image.url} />
+      </div>
+      <div className="product-info">
+        <h3>{product.title}</h3>
+        <div>${product.price}</div>
+        <div>Description: {product.description}</div>
+        <div>Uploaded on: {product.date}</div>
+        <div>By: {product.artist && product.artist.username}</div>
+      </div>
+    </div>
+  );
 }
