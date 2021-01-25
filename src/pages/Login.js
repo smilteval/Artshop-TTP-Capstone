a46cc3ed14b63b12c1f7b0eafc1b410b16757762
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-25 16:01:50 -0500

    Redirects user to home after successful login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,53 @@
-import React, {useState} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
+import {UserContext} from '../context/UserContext';
 
-export default function Login() {
+export default ({history}) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
+    const {user, setUser} = useContext(UserContext);
+    console.log("user ", user);
+
+    useEffect(() => {
+        if(user){
+            history.push('/');
+        }
+    }, [user])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
-        const response = await fetch ('https://ttp-art-store.herokuapp.com/auth/local', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                identifier: email,
-                password
+        try {
+            //Check if the user is authenticated
+            const response = await fetch ('https://ttp-art-store.herokuapp.com/auth/local', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    identifier: email,
+                    password
+                })
             })
-        })
+    
+            const data = await response.json();
+            console.log("data", data);
+            
+            //Display error if there is one
+            if(data.message){
+                setError(data.message[0].messages[0].message)
+    
+                return //Stop execution
+            }
 
-        const data = await response.json();
+            setUser(data);
 
-        console.log("data", data);
+        } catch(err){
+            setError('Something went wrong' + err)
+        }
+
+
     }
 
     return (
@@ -31,21 +56,36 @@ export default function Login() {
                 Login
             </h2>
 
+            {/* form for login */}
             <form onSubmit={handleSubmit}>
+
+                {/* email field */}
                 <input
                     type = 'email'
                     value = {email}
-                    onChange = {(event) => setEmail(event.target.value)}
+                    onChange = {(event) => {
+                        setError('');
+                        setEmail(event.target.value)
+                    }}
                 />
                 <br/>
+
+                {/* password field */}
                 <input
                     type = 'password'
                     value = {password}
-                    onChange = {(event) => setPassword(event.target.value)}
+                    onChange = {(event) => {
+                        setError('');
+                        setPassword(event.target.value)}
+                    }
                 />
                 <br/>
+
+                {/* submit button */}
                 <button>Login</button>
             </form>
+
+            {error && <p>{error}</p>}
 
         </div>
     )
