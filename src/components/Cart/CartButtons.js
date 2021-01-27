abef69eddc8910e5b3c9709adf26644fdd644f93
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-27 12:58:07 -0500

    Cart page functionality complete. Still needs styling.

--- src/components/Cart/CartButtons.js
+++ src/components/Cart/CartButtons.js
@@ -7,12 +7,13 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
+  const {total_items} = useCartContext()
     return <Wrapper className='cart-btn-wrapper'> 
         <Link to='/cart' className='cart-btn'>
-            CartButtons
+            Cart
             <span className = 'cart-container'>
                 <FaShoppingCart/>
-                <span className = 'cart-value'>12</span>
+                <span className = 'cart-value'>{total_items}</span>
 
             </span>
         </Link>
