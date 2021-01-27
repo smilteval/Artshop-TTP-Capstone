468a3424c9aebd3a8f4c7dec987bc63ac4dfea25
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-27 15:07:45 -0500

    added upload confirmation and more styling

--- src/components/PublishArt/ProductForm.js
+++ src/components/PublishArt/ProductForm.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import FilledInput from "@material-ui/core/FilledInput";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -7,6 +6,49 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import FormControl from "@material-ui/core/FormControl";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { green } from "@material-ui/core/colors";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[700],
+    },
+  },
+  fabProgress: {
+    color: green[500],
+    position: "absolute",
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 const categories = [
   {
@@ -31,7 +73,17 @@ export default function ProductForm() {
   const [imagePreview, setImagePreview] = useState("");
   const [image, setImage] = useState();
 
+  const classes = useStyles();
   const fileInputRef = useRef();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const timer = React.useRef();
+  const [openSuccess, setOpenSuccess] = React.useState(false);
+  const [openError, setOpenError] = React.useState(false);
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
 
   //image preview
   useEffect(() => {
@@ -66,7 +118,28 @@ export default function ProductForm() {
     );
     const data = await response.json();
 
+    if (response.status !== 200) {
+      setOpenError(true);
+    } else {
+      setOpenSuccess(true);
+    }
+
     console.log("data", data);
+    console.log("response", response);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenError(false);
+    setOpenSuccess(false);
   };
 
   return (
@@ -83,7 +156,7 @@ export default function ProductForm() {
                 fileInputRef.current.click();
               }}
             >
-              Please upload an image of your product
+              <i id="upload-img-text">Please upload an image of your product</i>
             </button>
           )}
 
@@ -103,17 +176,20 @@ export default function ProductForm() {
           />
         </div>
         <div id="product-info-upload">
+          <h3>Please provide information about your art</h3>
           <TextField
             label="Title"
             variant="filled"
+            size="small"
             required
             value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
-
+          <br></br>
           <TextField
             label="Description"
             variant="filled"
+            size="small"
             multiline
             rowsMax={5}
             rows={5}
@@ -121,11 +197,12 @@ export default function ProductForm() {
             value={description}
             onChange={(event) => setDescription(event.target.value)}
           />
-
+          <br></br>
           <FormControl fullWidth variant="filled">
             <InputLabel>Price*</InputLabel>
             <FilledInput
               type="number"
+              size="small"
               required
               value={price}
               onChange={(event) => setPrice(event.target.value)}
@@ -134,22 +211,48 @@ export default function ProductForm() {
               }
             />
           </FormControl>
-
+          <br></br>
           <TextField
+            label="Art category"
+            variant="filled"
+            helperText="Please select one category"
+            size="small"
             select
             required
-            label="Art category"
             value={category}
             onChange={(event) => setCategory(event.target.value)}
-            helperText="Please select one category"
-            variant="filled"
           >
             {categories.map((option) => (
               <MenuItem value={option.value}>{option.label}</MenuItem>
             ))}
           </TextField>
+          <br></br>
+          <Button
+          id="publish-btn"
+          type="submit"
+          variant="contained"
+          color="primary"
+          startIcon={<CloudUploadIcon />}
+        >
+          Publish
+        </Button>
         </div>
-        <Button type="submit">Publish</Button>
+        
+
+        <Snackbar
+          open={openSuccess}
+          autoHideDuration={6000}
+          onClose={handleClose}
+        >
+          <Alert onClose={handleClose} severity="success">
+            Your art was published successfully!
+          </Alert>
+        </Snackbar>
+        <Snackbar>
+          <Alert open={openError} onClose={handleClose} severity="error">
+            We couldn't upload your art, please try again...
+          </Alert>
+        </Snackbar>
       </form>
     </div>
   );
