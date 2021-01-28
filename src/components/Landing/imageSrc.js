651c0eb3023e80589e1eb58a997ce5fef4ec6267
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-27 23:27:02 -0500

    fixed cards on the landing page

--- src/components/Landing/imageSrc.js
+++ src/components/Landing/imageSrc.js
@@ -1,23 +1,25 @@
-import ImageOne from '../../assets/selling.jpg'
-import ImageTwo from '../../assets/Shopping.jpg'
-console.log(`url(${ImageOne})`)
+import ImageOne from "../../assets/selling.jpg";
+import ImageTwo from "../../assets/Shopping.jpg";
+console.log(`url(${ImageOne})`);
 const images = [
-    {
-title: 'Sell Your Art!',
-description: "Use our site to publish your art and sell to our users! You can also accept incoming special requests.",
-imageUrl: ImageOne,
-time: 1500,
-button: "Start Selling",
-link:'/home'
-    },
-    {
-        title: 'Buy Art!',
-        description: "Browse our site to check out the latest art submissions that you can buy and get sent directly to you! You are also able to select from our artist and request custom art.",
-        imageUrl: ImageTwo,
-        time: 1500,
-        button: "Start Buying",
-        link: '/home'
-            }
-]
+  {
+    title: "Sell Your Art!",
+    description:
+      "Use our site to publish your art and sell to our users! You can also accept incoming special requests. ",
+    imageUrl: ImageOne,
+    time: 1500,
+    button: "Start Selling",
+    link: "/home",
+  },
+  {
+    title: "Buy Art!",
+    description:
+      "Browse our site to check out the latest art submissions that you can buy and get sent directly to you! You are also able to select from our artist and request custom art.",
+    imageUrl: ImageTwo,
+    time: 1500,
+    button: "Start Buying",
+    link: "/home",
+  },
+];
 
 export default images;
--- src/components/Landing/imgCard.js
+++ src/components/Landing/imgCard.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
 //import Images from '../Landing/imageSrc'
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { CardActions, Collapse } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { CardActions, Collapse } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 645,
-    background: 'rgb(9 14 19 / 88%)',
-    margin: '20px',
+    background: "rgb(9 14 19 / 88%)",
+    margin: "20px",
   },
   media: {
     height: 400,
   },
   title: {
-    fontFamily: 'Lato',
-    fontWeight: 'bold',
-    fontSize: '2rem',
-    color: '#fff',
+    fontFamily: "Lato",
+    fontWeight: "bold",
+    fontSize: "2rem",
+    color: "#fff",
   },
   desc: {
     fontFamily: `"Lato"`,
-    fontWeight: 'bold',
-    fontSize: '1.1rem',
-    color: '#ddd',
+    fontWeight: "bold",
+    fontSize: "1.1rem",
+    color: "#ddd",
   },
   buttons: {
-    fontFamily: 'Lato',
-    fontWeight: 'bold',
-    color: '#F0EBD8',
-    backgroundColor: '#A37774',
-  }
+    fontFamily: "Lato",
+    fontWeight: "bold",
+    color: "#F0EBD8",
+    backgroundColor: "#A37774",
+  },
 });
 
 export default function ImageCard({ images, checked }) {
@@ -44,10 +44,7 @@ export default function ImageCard({ images, checked }) {
   return (
     <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})}>
       <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image={images.imageUrl}
-        />
+        <CardMedia className={classes.media} image={images.imageUrl} />
         <CardContent>
           <Typography
             gutterBottom
@@ -62,17 +59,22 @@ export default function ImageCard({ images, checked }) {
             color="textSecondary"
             component="p"
             className={classes.desc}
+            style={{ height: "100px" }}
           >
             {images.description}
           </Typography>
         </CardContent>
         <CardActions>
-        <Link to={images.link} >
-                 <Button size= 'large' color="secondary" className = {classes.buttons} >
-                {images.button}
-                
-                </Button> 
-                </Link> 
+          <Link to={images.link}>
+            <Button
+              style={{ margin: "5px", padding: "10px" }}
+              size="large"
+              color="secondary"
+              className={classes.buttons}
+            >
+              {images.button}
+            </Button>
+          </Link>
         </CardActions>
       </Card>
     </Collapse>
